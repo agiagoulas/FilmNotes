7aa1cb4e50a9c62c99e981a4590acd61596a951f
Alexander Giagoulas <alexander.giagoulas@ibm.com>
2020-04-22 11:05:38 +0200

    Initial Commit

---
+++ FilmNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmNotes
+//
+//  Created by Alexander Giagoulas on 22.04.20.
+//  Copyright © 2020 Alexander Giagoulas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
